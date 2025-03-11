fb3b9117dfb8974421ab2bb4e7470adc27056a91
Gabriel Kret <gabrielkret123@gmail.com>
2025-03-10 22:53:23 -0400

    prepped Data_Analysis_VIC_Data file for use in the pipeline.

--- Data_Pipeline/Data_Analysis_VIC_Data.py
+++ Data_Pipeline/Data_Analysis_VIC_Data.py
@@ -1,1 +1,20 @@
- 
+"""
+ Data Analysis Script for Module 3 Project
+Team A2: Digital Image Correlation
+Authors: Gabriel Kret, Maria Alvarado, Jonas Margono
+
+Course: ME-360-A
+Instructors: Dr. David Wootton, Dr. Kamau Wright
+Date: Spring 2025
+
+Explanation:
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import Preprocessing_VIC_Data as PP
+
+
